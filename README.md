34be2a496608ce65b04990a9eed7c73415a69145
natwar.patidar <natwar.patidar@gammaedge.io>
2025-09-25 16:21:13 -0400

    Add fixes and Readme

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
-# React + Vite
+## Paginated Table
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+### Features: 
+1. Fetches data from a free users api in paginated way and chache it using react-query
+2. Made a custom hook
+3. Used this hook to get the api data and rendered it on the ui using MUI table component
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-## React Compiler
-
-The React Compiler is not enabled on this template. To add it, see [this documentation](https://react.dev/learn/react-compiler/installation).
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+### Tools and technologies:
+1. React js
+2. @tanstack/react-query
+3. Axios
--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,14 +1,14 @@
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { useGetUsers } from '../../hooks/useGetUsers';
-import { useEffect, useState } from 'react';
-import TablePagination from '@mui/material/TablePagination';
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { useGetUsers } from "../../hooks/useGetUsers";
+import { useEffect, useState } from "react";
+import TablePagination from "@mui/material/TablePagination";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -21,76 +21,79 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
+  "&:nth-of-type(odd)": {
     backgroundColor: theme.palette.action.hover,
   },
   // hide last border
-  '&:last-child td, &:last-child th': {
+  "&:last-child td, &:last-child th": {
     border: 0,
   },
 }));
 
 export default function CustomizedTables() {
-    const [page, setPage] = useState(0);
-    const [limit, setLimit] = useState(10);
-    const [rows, setRows] = useState(null);
+  const [page, setPage] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [rows, setRows] = useState(null);
 
-    const { users, isSuccess } = useGetUsers({ page, limit });
-    console.log(users);
+  const { users, isSuccess } = useGetUsers({ page, limit });
+  console.log(users);
 
-    function handleChangePage (event, newPage) {
-        setPage(newPage);
+  function handleChangePage(event, newPage) {
+    setPage(newPage);
+  }
+
+  function handleChangeRowsPerPage(event) {
+    setLimit(+event.target.value);
+    setPage(1);
+  }
+
+  useEffect(() => {
+    if (isSuccess) {
+      setRows(users?.data);
     }
+  }, [isSuccess, users]);
 
-    function handleChangeRowsPerPage (event) {
-        setLimit(+event.target.value);
-        setPage(1);
-    }
-    
-    useEffect(() => {
-        if(isSuccess) {       
-            setRows(users?.data)
-        }
-    }, [isSuccess, users]);
-
-    return (
-        <Paper sx={{ width: '100%' }}>
-            <TableContainer sx={{ maxHeight: 850 }}>
-                <Table sx={{ minWidth: 700 }} stickyHeader aria-label="sticky table">
-                    <TableHead>
-                        <TableRow>
-                            <StyledTableCell>Picture</StyledTableCell>
-                            <StyledTableCell align="right">Name</StyledTableCell>
-                            <StyledTableCell align="right">Email&nbsp;(g)</StyledTableCell>
-                            <StyledTableCell align="right">Phone&nbsp;(g)</StyledTableCell>
-                            <StyledTableCell align="right">Gender&nbsp;(g)</StyledTableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        { rows && rows.map((row) => (
-                                <StyledTableRow key={row.id}>
-                                    <StyledTableCell component="th" scope="row">
-                                        <img src={row.picture.medium} />
-                                    </StyledTableCell>
-                                    <StyledTableCell align="right">{row.name.title} {row.name.first} {row.name.last}</StyledTableCell>
-                                    <StyledTableCell align="right">{row.email}</StyledTableCell>
-                                    <StyledTableCell align="right">{row.phone}</StyledTableCell>
-                                    <StyledTableCell align="right">{row.gender}</StyledTableCell>
-                                </StyledTableRow>
-                            )
-                        )}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <TablePagination
-                rowsPerPageOptions={[5, 10, 20]}
-                component="div"
-                count={500}
-                rowsPerPage={limit}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-            />
-        </Paper>
-    );
+  return (
+    <>
+      <Paper sx={{ width: "100%" }}>
+        <TableContainer sx={{ maxHeight: 900 }}>
+          <Table sx={{ minWidth: 700 }} stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell align="left">Picture</StyledTableCell>
+                <StyledTableCell align="left">Name</StyledTableCell>
+                <StyledTableCell align="left">Email</StyledTableCell>
+                <StyledTableCell align="left">Phone</StyledTableCell>
+                <StyledTableCell align="left">Gender</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows?.map((row) => (
+                <StyledTableRow key={row.id}>
+                  <StyledTableCell component="th" scope="row">
+                    <img src={row.picture.thumbnail} className="w-10 h-10 rounded-full" />
+                  </StyledTableCell>
+                  <StyledTableCell align="left">
+                    {row.name.title} {row.name.first} {row.name.last}
+                  </StyledTableCell>
+                  <StyledTableCell align="left">{row.email}</StyledTableCell>
+                  <StyledTableCell align="left">{row.phone}</StyledTableCell>
+                  <StyledTableCell align="left">{row.gender}</StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 20]}
+        component="div"
+        count={500}
+        rowsPerPage={limit}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </>
+  );
 }
